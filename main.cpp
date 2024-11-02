fe219ecca02fce82a024e9047b9396a606d26934
= <neagumario21@yahoo.com>
2024-11-02 16:45:25 +0200

    Iterate days

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ const std::string API_KEY = "7bcd6334-bc2e-4cbf-b9d4-61cb9e868869";
 
 int main()
 {
+    Map map{};
+    map.parse();
+
     std::string session_id;
 
     cpr::Response r = cpr::Post(cpr::Url{"192.168.123.221:8080/api/v1/session/start"},
@@ -18,27 +21,38 @@ int main()
     {
         std::cout << r.status_code << '\n';
         std::cout << r.text << '\n';
+        return EXIT_FAILURE;
     }
     session_id = r.text;
+    std::cout << "SESSION CREATED\n";
 
-    r = cpr::Post(cpr::Url{"192.168.123.221:8080/api/v1/play/round"},
-                  cpr::Header{{"API-KEY",      API_KEY},
-                              {"SESSION-ID",   session_id},
-                              {"Content-Type", "application/json"},
-                  },
-                  cpr::Body{"{\n"
-                            "  \"day\": 0,\n"
-                            "  \"movements\": [\n"
-                            "    {\n"
-                            "      \"connectionId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n"
-                            "      \"amount\": 0\n"
-                            "    }\n"
-                            "  ]\n"
-                            "}"});
-    if (r.status_code != 200)
+    for (int day = 0; day <= 41; ++day)
     {
-        std::cout << r.status_code << '\n';
-        std::cout << r.text << '\n';
+        nlohmann::json body;
+        body["day"] = day;
+        body["movements"] = nlohmann::json::array();
+
+        nlohmann::json movement;
+        movement["connectionId"] = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
+        movement["amount"] = 5;
+
+        body["movements"].push_back(movement);
+
+        r = cpr::Post(cpr::Url{"192.168.123.221:8080/api/v1/play/round"},
+                      cpr::Header{{"API-KEY",      API_KEY},
+                                  {"SESSION-ID",   session_id},
+                                  {"Content-Type", "application/json"},
+
+                      }, cpr::Body{to_string(body)});
+
+
+
+        if (r.status_code != 200)
+        {
+            std::cout << r.status_code << '\n';
+            std::cout << r.text << '\n';
+            return EXIT_FAILURE;
+        }
     }
 
     r = cpr::Post(cpr::Url{"192.168.123.221:8080/api/v1/session/end"},
@@ -47,12 +61,9 @@ int main()
     {
         std::cout << r.status_code << '\n';
         std::cout << r.text << '\n';
+        return EXIT_FAILURE;
     }
-    std::cout << "SESSION CREATED AND CLOSED SUCCESSFULLY\n";
+    std::cout << "SESSION CLOSED\n";
 
-    Map map{};
-    map.parse();
-    map.print();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
