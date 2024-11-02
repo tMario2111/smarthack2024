2ed96f56431ce7257b5eb43ce0df123711a966f9
= <neagumario21@yahoo.com>
2024-11-02 14:11:59 +0200

    HTTP request

--- main.cpp
+++ main.cpp
@@ -2,14 +2,51 @@
 #include <string>
 
 #include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
 
 const std::string API_KEY = "7bcd6334-bc2e-4cbf-b9d4-61cb9e868869";
 
 int main()
 {
-    cpr::Response r = cpr::Get(cpr::Url("192.168.123.221:8090/api-docs"));
-    std::cout << r.status_code << '\n';
-    std::cout << r.text << '\n';
+    std::string session_id;
+
+    cpr::Response r = cpr::Post(cpr::Url{"192.168.123.221:8080/api/v1/session/start"},
+                                cpr::Header{{"API-KEY", API_KEY}});
+    if (r.status_code != 200)
+    {
+        std::cout << r.status_code << '\n';
+        std::cout << r.text << '\n';
+    }
+    session_id = r.text;
+    std::cout << "ID: " << session_id << '\n';
+
+    r = cpr::Post(cpr::Url{"192.168.123.221:8080/api/v1/play/round"},
+                  cpr::Header{{"API-KEY",      API_KEY},
+                              {"SESSION-ID",   session_id},
+                              {"Content-Type", "application/json"},
+                  },
+                  cpr::Body{"{\n"
+                            "  \"day\": 0,\n"
+                            "  \"movements\": [\n"
+                            "    {\n"
+                            "      \"connectionId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n"
+                            "      \"amount\": 0\n"
+                            "    }\n"
+                            "  ]\n"
+                            "}"});
+    if (r.status_code != 200)
+    {
+        std::cout << r.status_code << '\n';
+        std::cout << r.text << '\n';
+    }
+
+    r = cpr::Post(cpr::Url{"192.168.123.221:8080/api/v1/session/end"},
+                  cpr::Header{{"API-KEY", API_KEY}});
+    if (r.status_code != 200)
+    {
+        std::cout << r.status_code << '\n';
+        std::cout << r.text << '\n';
+    }
 
     return 0;
 }
